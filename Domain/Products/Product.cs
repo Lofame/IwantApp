b50a1c40b9dcf8cac692bb91980bd114669be20e
Luando <lofame@gmail.com>
2022-08-21 17:05:32 -0300

    first commit

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product
+{
+    //public Guid Id { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public int Description { get; set; }
+    public bool HasStock { get; set; }
+
+}
